88a2dc506fd730f979bd085364d33c61283613c7
zang3tsu88 <maximenya.a@gmail.com>
2022-12-15 20:48:39 +0300

    Reorganize css to bem methodology

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ const popup = document.querySelector(".popup");
 
 // Buttons
 const openProfilePopup = document.querySelector(".profile__edit-btn");
-const closeProfilePopup = document.querySelector(".popup_close-btn");
+const closeProfilePopup = document.querySelector(".popup__close-btn");
 const submitButton = document.querySelector(".popup__submit-btn");
 
 // Open and Close Pop-up
@@ -28,16 +28,12 @@ const formOccupation = popup.querySelector(".popup__occupation");
 // Form Submit
 function formSubmit(evt) {
   evt.preventDefault(); // не много не понял, работает нормально и без этого
-  userName.textContent = formName.value;  // почему formName показывает что он let, когда он const.
+  userName.textContent = formName.value;
   userOccupation.textContent = formOccupation.value;
   closePopup();
   formName.value = "";          //  после закрытия формы, очищает поле ввода,
   formOccupation.value = "";    //  что бы при следующем открытии не отображалось ничего кроме placeholder
 }
-
-// submitButton.addEventListener("click", () => {
-  //   userName.value = formName.value;
-  // })
 
 submitButton.addEventListener("click", formSubmit);
 closeProfilePopup.addEventListener("click", closePopup);
