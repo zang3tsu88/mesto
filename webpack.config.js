7efad197d7207f2e2af6054f801865e5a8a98c91
zang3tsu88 <maximenya.a@gmail.com>
2023-03-15 02:22:34 +0300

    final build

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+
+  entry: { main: "./src/pages/index.js" },
+
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
+    publicPath: "",
+  },
+
+  devServer: {
+    static: path.resolve(__dirname, "./dist"),
+    compress: true,
+    port: 8080,
+    open: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: "/node_modules",
+      },
+
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: { importLoaders: 1 },
+          },
+          "postcss-loader",
+        ],
+      },
+
+      {
+        test: /\.(png|svg|jpg|gif|webp)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[name].[hash][ext]",
+        },
+      },
+
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "fonts/[name].[hash][ext]",
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./src/index.html" }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+  ],
+};
